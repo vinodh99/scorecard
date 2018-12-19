457e8ef244429c6c86488e9afc81358ece8f9802
vinodh tankasala <vinodh2019@gmail.com>
2018-12-19 17:55:12 -0500

    added content to cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 import { Card, Row, Col } from 'antd';
+import './cards.css'
 import './carousal.css'
 const { Meta } = Card;
 
 class Cards extends Component {
 
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
+            coi: 0,
+            rpy: 0,
+            fpy: 0,
+            gtm: 0,
+            gql: 0,
+            ghc1: 0,
+            ghc2: 0,
+            gfx: 0,
+            rpm: 0,
+            ias: 0,
+            ish: 0,
+            das: 0,
+            ds: 0,
+            aar: 0,
 
-        };
+        }
     }
 
 
     render() {
+
         return (
-            <div style={{ background: '#ECECEC', height: 620, }} >
+            <div style={{ background: '#ECECEC', height: 620 }} >
                 <Row>
                     <Col span={20}>
                         <Row gutter={16} style={{ paddingBottom: 10, paddingTop: 10, paddingLeft: 10 }}>
                             <Col span={12}>
-                                <Card style={{ background: '#7D6DFB', textAlign: 'center', height: 200 }} bordered={false}>
-                                    <h1>GTM</h1>
+                                <Card className='card' style={{ background: '#7D6DFB', textAlign: 'center', height: 200 }} bordered={false}>
+                                    <p >
+                                        Revenue gains from accelerated time to market of new functionality (GTM)
+                                    </p>
+                                    <h1>
+                                        GTM:
+                                    </h1>
                                 </Card>
                             </Col>
                             <Col span={12}>
                                 <Card style={{ background: '#35D385', textAlign: 'center', height: 200 }} bordered={false} >
-                                    <h1>GQL</h1>
+                                    <p>Gains from cost reduction of application failures resulting from increased quality (GQL)</p>
+                                    <h1>GQL:</h1>
                                 </Card>
                             </Col>
 
@@ -34,24 +56,31 @@ class Cards extends Component {
                         <Row gutter={16} style={{ paddingLeft: 10 }}>
                             <Col span={12}>
                                 <Card style={{ background: '#FECA4F', textAlign: 'center', height: 200 }} bordered={false}>
+                                    <p>
+                                        Gains from enhanced IT team productivity and cost reduction of IT headcount waste (GHC)
+                                    </p>
                                     <h2>
-                                        GHC (IT Ops)
-                            </h2>
+                                        GHC (IT Ops):
+                                    </h2>
                                     <h2>
-                                        GHC (Developers)
-                            </h2>
+                                        GHC (Developers):
+                                    </h2>
                                 </Card>
                             </Col>
                             <Col span={12}>
                                 <Card style={{ background: '#33CCF4', textAlign: 'center', height: 200 }} bordered={false}>
-                                    <h1>GFX</h1>
+                                    <p>Gain from flexibility in the IT environment (GFX)</p>
+                                    <h1>GFX:</h1>
                                 </Card>
                             </Col>
                         </Row>
                         <Row gutter={16} style={{ paddingTop: 10, paddingLeft: 10 }}>
                             <Col>
                                 <Card style={{ background: '#87460a', textAlign: 'center', height: 180, justifyContent: 'center' }} bordered={false}>
-                                    <h1>ROI</h1>
+                                    <p>
+                                        Return Of Investment
+                                </p>
+                                    <h1>ROI:</h1>
                                 </Card>
                             </Col>
                         </Row>
--- src/components/scorecard.js
+++ src/components/scorecard.js
@@ -33,79 +33,81 @@ class DashBoard extends Component {
             </Menu>
         );
         return (
-            <Layout style={{ minHeight: '100vh' }}>
-                <Sider>
-                    <Menu defaultSelectedKeys={['1']} mode="inline">
-                        <img src="https://rsrit.com/wp-content/uploads/2017/12/logo_dark.png" alt="image" width="200px" height="70px"></img>
+            <div >
+                <Layout style={{ minHeight: '100vh' }}>
+                    <Sider>
+                        <Menu defaultSelectedKeys={['1']} mode="inline">
+                            <img src="https://rsrit.com/wp-content/uploads/2017/12/logo_dark.png" alt="image" width="200px" height="70px"></img>
 
-                        <SubMenu
-                            key="sub1"
-                            title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GTM</span>}
-                        >
-                            <Menu.Item key="1"  >Revenue/Year: 20</Menu.Item>
-                            <Menu.Item key="2" >
-                                <Row>
-                                    Revenue inc estimate
+                            <SubMenu
+                                key="sub1"
+                                title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GTM</span>}
+                            >
+                                <Menu.Item key="1"  >Revenue/Year: 20</Menu.Item>
+                                <Menu.Item key="2" >
+                                    <Row>
+                                        Revenue inc estimate
                             </Row>
-                                <Row>
-                                    20
+                                    <Row>
+                                        20
                             </Row>
-                            </Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub2"
-                            title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GQL</span>}
-                        >
-                            <Menu.Item key="1"  >Failures/Year: 20</Menu.Item>
-                            <Menu.Item key="2" >Revenue/minute</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub3"
-                            title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GHC (IT Ops)</span>}
-                        >
-                            <Menu.Item key="1"  >IT Average Salary</Menu.Item>
-                            <Menu.Item key="2" >IT staff Headcount</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub4"
-                            title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GHC (Developers)</span>}
-                        >
-                            <Menu.Item key="1"  >Develper Avg salary</Menu.Item>
-                            <Menu.Item key="2" >Developer Staff</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub5"
-                            title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GFX</span>}
-                        >
-                            <Menu.Item key="1"  >App. Annual Revenue</Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }} >
-                        <Row>
-                            <Col span={12}>
-                                <h1 style={{ fontWeight: 'bold', color: '#0066c', fontFamily: 'Cochin', textAlign: 'right' }}>SCORECARD</h1>
-                            </Col>
-                            <Col span={12}>
-                                <Dropdown overlay={data} trigger={['click']}>
-                                    <h1 className="ant-dropdown-link" style={{ paddingRight: 40, fontWeight: 'bold', color: '#0066c', fontFamily: 'Cochin', textAlign: 'right' }}>
-                                        <Icon className="custom-icon" type="setting" />
-                                    </h1>
-                                </Dropdown>
+                                </Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GQL</span>}
+                            >
+                                <Menu.Item key="1"  >Failures/Year: 20</Menu.Item>
+                                <Menu.Item key="2" >Revenue/minute</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub3"
+                                title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GHC (IT Ops)</span>}
+                            >
+                                <Menu.Item key="1"  >IT Average Salary</Menu.Item>
+                                <Menu.Item key="2" >IT staff Headcount</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub4"
+                                title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GHC (Developers)</span>}
+                            >
+                                <Menu.Item key="1"  >Develper Avg salary</Menu.Item>
+                                <Menu.Item key="2" >Developer Staff</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub5"
+                                title={<span style={{ textAlign: 'right' }}><Icon type="sliders" />GFX</span>}
+                            >
+                                <Menu.Item key="1"  >App. Annual Revenue</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }} >
+                            <Row>
+                                <Col span={12}>
+                                    <h1 style={{ fontWeight: 'bold', color: '#0066c', fontFamily: 'Cochin', textAlign: 'right' }}>SCORECARD</h1>
+                                </Col>
+                                <Col span={12}>
+                                    <Dropdown overlay={data} trigger={['click']}>
+                                        <h1 className="ant-dropdown-link" style={{ paddingRight: 40, fontWeight: 'bold', color: '#0066c', fontFamily: 'Cochin', textAlign: 'right' }}>
+                                            <Icon className="custom-icon" type="setting" />
+                                        </h1>
+                                    </Dropdown>
 
-                            </Col>
+                                </Col>
 
-                        </Row>
+                            </Row>
 
-                    </Header>
-                    <Content style={{ alignItems: 'center', margin: '24px 16px', padding: 24, background: '#fff', minHeight: 580 }}>
-                        <Cards {...this.props} />
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>
-                    </Footer>
-                </Layout>
-            </Layout >
+                        </Header>
+                        <Content style={{ alignItems: 'center', margin: '24px 16px', padding: 24, background: '#fff', minHeight: 580 }}>
+                            <Cards {...this.props} />
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>
+                        </Footer>
+                    </Layout>
+                </Layout >
+            </div>
         );
     }
 }
