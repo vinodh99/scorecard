d902e700aaa9570b6e2aa50c3efb7353de49e9bc
vinodh tankasala <vinodh2019@gmail.com>
2018-12-19 09:34:21 -0500

    Updates

---
+++ precache-manifest.3c155e2e97a06cafaaea2065498bf2f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1963a2ce67225f754d76",
+    "url": "/static/js/main.1963a2ce.chunk.js"
+  },
+  {
+    "revision": "dbb2494434dea7807ed8",
+    "url": "/static/js/1.dbb24944.chunk.js"
+  },
+  {
+    "revision": "1963a2ce67225f754d76",
+    "url": "/static/css/main.6d6acdba.chunk.css"
+  },
+  {
+    "revision": "dbb2494434dea7807ed8",
+    "url": "/static/css/1.d3fb8ca2.chunk.css"
+  },
+  {
+    "revision": "4a8df243a9ae6fcce7a076b345b27f73",
+    "url": "/index.html"
+  }
+];
